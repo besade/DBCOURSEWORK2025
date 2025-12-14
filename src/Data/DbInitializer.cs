59077c2eeb06e948c79968883fff7b8b9cf74124
besade <artv2706@gmail.com>
2025-12-15 01:36:24 +0200

    Added admin user

---
+++ src/Data/DbInitializer.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shop.Data;
+using Shop.Models;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Shop.Data
+{
+    public static class DbInitializer
+    {
+        public static void SeedAdmin(ApplicationDbContext context)
+        {
+            if (context.Customers.Any(c => c.IsAdmin))
+                return;
+
+            using var hmac = new HMACSHA512();
+            var password = "12345678";
+
+            var admin = new Customer
+            {
+                FirstName = "Admin",
+                LastName = "Admin",
+                Email = "admin@gmail.com",
+                PhoneNumber = "0000000000",
+                DateOfBirth = new DateOnly(2000, 1, 1),
+                IsAdmin = true,
+                PasswordSalt = hmac.Key,
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password))
+            };
+
+            context.Customers.Add(admin);
+            context.SaveChanges();
+        }
+    }
+}
--- src/Program.cs
+++ src/Program.cs
@@ -36,6 +36,7 @@ using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
     db.Database.Migrate();
+    DbInitializer.SeedAdmin(db);
 }
 
 app.UseHttpsRedirection();
