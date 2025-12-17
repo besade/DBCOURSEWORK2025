30a09ac5799ce98967063b896523a79ae072ae60
besade <artv2706@gmail.com>
2025-12-17 11:51:11 +0200

    DTOs update

---
+++ src/DTOs/CategorySalesDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace Shop.DTOs
+{
+    public class CategorySalesDto
+    {
+        public string CategoryName { get; set; } = null!;
+        public int TotalQuantitySold { get; set; }
+        public decimal TotalRevenue { get; set; }
+    }
+}
---
+++ src/DTOs/CustomerSpendingDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace Shop.DTOs
+{
+    public class CustomerSpendingDto
+    {
+        public string Email { get; set; } = null!;
+        public int OrdersCount { get; set; }
+        public decimal TotalSpent { get; set; }
+    }
+}
